7374030129ff4efe425984dd2f2323ddfc8905b1
Erica Wright <ericaw21@gmail.com>
2017-05-16 11:23:28 -0700

    update login to display form to choose images

--- sites/all/themes/custom/page.tpl.php
+++ sites/all/themes/custom/page.tpl.php
@@ -79,7 +79,7 @@
       <div id="mainmenusection">
         <?php if ($main_menu || $secondary_menu): ?>
           <div id="navigation"><div class="section">
-            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
             <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
           </div></div> <!-- /.section, /#navigation -->
         <?php endif; ?>
