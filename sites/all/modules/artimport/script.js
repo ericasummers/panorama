d21195d7f319fae66c2cf1d8899d068d93272db4
Erica Wright <ericaw21@gmail.com>
2017-05-18 14:07:53 -0700

    add jquery on image button clicks

---
+++ sites/all/modules/artimport/script.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+  $('.love').click(function(){
+      alert('This saves this drawing to your user profile favorites!');
+  });
+  $('.save-artist').click(function(){
+      alert('This saves the artist name to your user profile!');
+  });
+});
